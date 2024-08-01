d894802df618dcb83634ea5262ee7be375fab21e
lemeote <dorastiku@gmail.com>
2024-08-01 04:26:20 -0400

    update/ UI-&-component-of-item

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,63 +1,84 @@
 "use client"
 
+import Image from "next/image"
+
 import Header from "@/components/Header"
 import NotiItem from "@/components/noti-item"
-import { user_avatar, diamond_icon } from "../../../assets"
+import { ico_chart, ico_earning, ico_flower, ico_gift, ico_medal, ico_prize, ico_warining, ico_bell } from "../../../assets"
 
 const name = "Alison williams"
 
 const notiList = [
   {
-    notiIcon: user_avatar,
+    notiIcon: ico_chart,
     title: 'Congulations!',
     content: `Your friend ${name} has successfully joined through your referral link! You've earned 50 gems.`,
     unread: true,
     alerttime: "7 min ago",
   },
   {
-    notiIcon: user_avatar,
+    notiIcon: ico_earning,
     title: 'Congulations!',
     content: `Your friend ${name} has successfully joined through your referral link! You've earned 50 gems.`,
     unread: true,
     alerttime: "1 hour ago",
   },
   {
-    notiIcon: user_avatar,
+    notiIcon: ico_flower,
     title: 'Congulations!',
     content: `Your friend ${name} has successfully joined through your referral link! You've earned 50 gems.`,
     unread: false,
     alerttime: "now",
   },
   {
-    notiIcon: user_avatar,
+    notiIcon: ico_gift,
     title: 'Referral Milestone Reached',
     content: `Your friend ${name} has successfully joined through your referral link! You've earned 50 gems.`,
     unread: false,
     alerttime: "7 min ago",
   },
   {
-    notiIcon: user_avatar,
+    notiIcon: ico_medal,
     title: 'Congulations!',
     content: `Your friend ${name} has successfully joined through your referral link! You've earned 50 gems.`,
     unread: true,
     alerttime: "7 min ago",
   },
   {
-    notiIcon: user_avatar,
+    notiIcon: ico_prize,
     title: "You've Earned Rakeback",
     content: `You've earned 10% rakeback from ${name} activities this week, totaling 100 gems`,
     unread: false,
     alerttime: "7 min ago",
   },
+  {
+    notiIcon: ico_warining,
+    title: 'Referral Milestone Reached',
+    content: `Your friend ${name} has successfully joined through your referral link! You've earned 50 gems.`,
+    unread: false,
+    alerttime: "7 min ago",
+  },
 ]
 
+const BodyHeader = () => {
+  return (
+    <div className="flex justify-between w-full mt-6">
+      <div className="flex items-center justify-center gap-2">
+        <Image src={ico_bell} alt=""/>
+        <h2>Notifications</h2>
+      </div>
+        <span className="font-normal text-xs">Mark all as read</span>
+    </div>
+  );
+};
 
 const NotificationPage = () => {
   return (
     <>
-      <div className="w-full bg-bgcolor">
+      <div className="w-full bg-bgcolor text-white">
         <div className="p-6">
-          <Header imageSrc={diamond_icon} />
+          <Header imageSrc={ico_chart} />
+          <BodyHeader />
         </div>
         <main>
           {
--- src/components/noti-item.tsx
+++ src/components/noti-item.tsx
@@ -10,16 +10,21 @@ interface NotiItemProps {
 
 const NotiItem: React.FC<NotiItemProps> = ({ notiIcon, title, content, unread, alerttime }) => {
   return (
-    <div className={`p-6 flex gap-3 text-white ${unread === true ? 'bg-basic border border-border-color' : ''}`}>
-      <div className="flex justify-center items-center !w-[34px] !h-[34px] rounded-full bg-[#31254C]">
-        <Image src={notiIcon} alt="" width={16} height={16} />
+    <div className={`p-6 flex w-full justify-between gap-3 text-white hover:bg-button ${unread === true ? 'bg-basic border border-border-color' : ''}`}>
+      <div className="flex justify-center p-2 h-full items-center rounded-full bg-[#31254C]">
+        <div style={{ width: 16, height: 17 }}>
+          <Image src={notiIcon} alt="" width={16} height={16} />
+        </div>
       </div>
       <div className="text-left">
         <div className="flex justify-between w-full">
           <h1>{title}</h1>
-          <span className="text-xs text-[#b9b9b933]">{alerttime}</span>
+          <div className="flex justify-center items-center gap-1">
+            <div className={`${unread === true ? 'w-2 h-2 rounded-full bg-[#7819F3]':'hidden'}`}></div>
+            <span className="text-xs text-[#b9b9b933]">{alerttime}</span>
+          </div>
         </div>
-        <span className="text-[14px]">
+        <span className="text-[14px] text-[#b9b9b933]">
           {content}
         </span>
       </div>
