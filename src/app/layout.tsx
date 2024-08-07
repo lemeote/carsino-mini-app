e0747e6dca1acf9fca642edfa86002210b58474d
lemeote <dorastiku@gmail.com>
2024-08-07 20:54:46 +0900

    update-viewport-info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
+  height: "device-height",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
