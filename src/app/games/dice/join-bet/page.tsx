43dbf6f0f06a677bf46a0a7f56d2e347d8804d6e
lemeote <dorastiku@gmail.com>
2024-08-14 16:43:26 +0900

    update-join-bet-UI

---
+++ src/app/games/dice/join-bet/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { useState } from "react";
+
+import Image from "next/image";
+import { diamond_icon, ico_voice, ico_question } from "@assets";
+import Header from "@/components/Header";
+import { JoinBet } from "@/components/games-dice-bet-item";
+
+const DiceHeader = () => {
+  return (
+    <div className="flex justify-between w-full gap-2">
+      <span className="text-lg font-bold">Dice Battle</span>
+      <div className="flex gap-2 items-center">
+        <div className="flex justify-center items-center bg-inherit border-border-color border w-6 h-6 rounded-full">
+          <Image src={ico_voice} alt="" />
+        </div>
+        <div className="flex justify-center items-center border border-border-color gap-1 px-2 py-1 rounded-md">
+          <Image src={ico_question} alt="" />
+          <span className="text-[#584D74] font-bold leading-[18px] text-center">
+            How to play
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Main = () => {
+  const [close, setClose] = useState<boolean>(true);
+  const joinBetClose = () => {
+    setClose(false);
+  };
+
+  const joinBet = () => {};
+  return (
+    <div className="w-full h-full min-h-screen text-white text-xs bg-bgcolor pb-6">
+      <header>
+        <Header imageSrc={diamond_icon} />
+      </header>
+      <main className="px-6 mt-6">
+        <div className="relative">
+          <DiceHeader />
+          <div className="flex flex-col gap-4 mt-6">
+            <div className="w-full h-40 rounded-3xl bg-basic p-4">
+              <span>Player 1</span>
+            </div>
+            <div className="w-full h-40 rounded-3xl bg-basic p-4">
+              <span>player 2</span>
+            </div>
+          </div>
+        </div>
+        {close && <JoinBet joinBet={joinBet} joinBetClose={joinBetClose} />}
+      </main>
+    </div>
+  );
+};
+
+export default Main;
--- src/app/games/dice/page.tsx
+++ src/app/games/dice/page.tsx
@@ -2,9 +2,9 @@
 
 import Image from "next/image";
 
-import { user1, user2, ico_battle, diamond_icon, ico_filter } from "@assets";
-import { Icons } from "@/components/Icons";
+import { ico_battle, diamond_icon, ico_filter } from "@assets";
 import Header from "@/components/Header";
+import { BetItem } from "@/components/games-dice-bet-item";
 
 import {
   Select,
@@ -14,14 +14,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { DICE_BET_LIST } from "@temp";
+
+interface ActiveBattleProps {
+  count: number;
+}
 
 const SelectDemo = () => {
   return (
     <Select>
       <SelectTrigger className="bg-bgcolor text-white">
         <div className="flex gap-2 text-[#584D74]">
-        <Image src={ico_filter} alt="" />
-        <SelectValue defaultValue={"High"} placeholder="High" />
+          <Image src={ico_filter} alt="" />
+          <SelectValue defaultValue={"High"} placeholder="High" />
         </div>
       </SelectTrigger>
       <SelectContent>
@@ -35,10 +40,10 @@ const SelectDemo = () => {
   );
 };
 
-const DiceHeader = () => {
+const DiceHeader: React.FC<ActiveBattleProps> = ({ count }) => {
   return (
     <div className="flex flex-wrap justify-evenly w-full gap-2">
-      <span className="text-lg font-bold">12 Active battle</span>
+      <span className="text-lg font-bold">{`${count} Active battle`}</span>
       <div className="flex gap-2 items-center">
         <div className="flex justify-center items-center bg-inherit border-border-color border rounded-full">
           <SelectDemo />
@@ -52,58 +57,6 @@ const DiceHeader = () => {
   );
 };
 
-const BetItem = () => {
-  return (
-    <div className="bg-basic border border-border-color rounded-3xl p-5 flex flex-wrap justify-evenly items-center gap-2">
-      <div className="flex justify-between gap-1 items-center">
-        <div className="flex flex-col gap-1 justify-center items-center w-11">
-          <div className="w-8 h-8">
-            <Image
-              src={user1}
-              alt="user1"
-              width={32}
-              height={32}
-              className="border rounded-full"
-            />
-          </div>
-          <span>Nenson</span>
-        </div>
-        <span className="text-sm text-gray-500 leading-4 italic font-extrabold">
-          VS
-        </span>
-        <div className="flex flex-col gap-1 justify-center items-center w-11">
-          <div className="w-8 h-8">
-            <Image
-              src={user2}
-              alt="user1"
-              width={32}
-              height={32}
-              className="border rounded-full"
-            />
-          </div>
-          <span>Tervor</span>
-        </div>
-      </div>
-      <div className="flex flex-col justify-between gap-2 items-center">
-        <span className="text-sm font-bold leading-4 text-gray-500">01/05</span>
-        <span>Round</span>
-      </div>
-      <div className="flex flex-col justify-center gap-2 items-end">
-        <div className="py-2 px-3 bg-button border border-border-color rounded-xl">
-          View bet
-        </div>
-        <div className="flex gap-1 items-center">
-          <span className="text-xs text-gray-500 leading-4 font-semibold">
-            Bet:
-          </span>
-          <span>100000</span>
-          <Icons.headerButton.diamond_amount />
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const Dice = () => {
   return (
     <div className="w-full h-full min-h-screen text-white text-xs bg-bgcolor pb-6">
@@ -111,10 +64,10 @@ const Dice = () => {
         <Header imageSrc={diamond_icon} />
       </header>
       <main className="px-6 mt-6">
-        <DiceHeader />
+        <DiceHeader count={13} />
         <div className="flex flex-col gap-4 min-h-screen w-full h-full mt-6">
-          {Array.from({ length: 10 }, (_, index) => (
-            <BetItem />
+          {DICE_BET_LIST.map((item, index) => (
+            <BetItem user1={item.user1} user2={item.user2} key={index} />
           ))}
         </div>
       </main>
